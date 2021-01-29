6177d253e4f0ba3bb555df0aa7b8cb7bf1e80715
donthamon <donovan.hollingsworth@gmail.com>
2021-01-29 00:51:13 -0500

    Signed-off-by: donthamon <donovan.hollingsworth@gmail.com>

--- Source/Start.bat
+++ Source/Start.bat
@@ -1,2 +1,15 @@
 Select from List
 bash Start.bat
+
+Prerequisites
+Docker 19.03 or later
+
+You can install the Compose CLI using the install script:
+
+curl -L https://raw.githubusercontent.com/docker/compose-cli/main/scripts/install/install_linux.sh | sh
+
+gh repo clone docker/compose-cli
+
+To enable using the local Docker Engine and to use existing Docker contexts, you will need to have the existing Docker CLI as com.docker.cli somewhere in your PATH. You can do this by creating a symbolic link from the existing Docker CLI.
+
+ln -s /path/to/existing/docker /directory/in/PATH/com.docker.cli
